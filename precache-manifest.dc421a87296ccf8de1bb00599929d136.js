1954d310cc726ee02e19da670c4a9a9e88f693f9
alexscub <alexscub1@gmail.com>
2019-10-26 13:46:54 +0300

    Updates

---
+++ precache-manifest.dc421a87296ccf8de1bb00599929d136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2154ea1cd2e43e06b22965df1df64734",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "56136403b9d3517a2321",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "4a55b6ceba6b37643d3d",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.a5708d84.chunk.css"
+  },
+  {
+    "revision": "56136403b9d3517a2321",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.cd37e070.chunk.js"
+  },
+  {
+    "revision": "4a55b6ceba6b37643d3d",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.f75a8295.chunk.js"
+  },
+  {
+    "revision": "512545c0587abd5294e2",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.ece2e0f6.js"
+  }
+]);
